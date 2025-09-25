ec9bfcd5151bebf61a4fbc8c6e05a5e01aa8e795
Akash <therealakash13@gmail.com>
2025-09-25 09:51:55 +0000

    search result, geolocation and weather card

--- Weatherly - A Weather App/weather.js
+++ Weatherly - A Weather App/weather.js
@@ -1,6 +1,14 @@
 var apiKey = "08655e017ab0c6c9c0f30f544c67abb8";
 var city = "Delhi";
 var lang = "hi";
+var searchRes = {
+  name: "",
+  local_names: {},
+  lat: 0,
+  lon: 0,
+  country: "",
+  state: "",
+};
 
 $(function () {
   $("#location").on("input", function () {
@@ -8,43 +16,97 @@ $(function () {
       $("#search_btn").addClass("disabled");
       $("#search").removeClass("end");
       $("#results").addClass("hidden");
+      $("#resp").addClass("hidden");
     } else if ($(this).val().trim().length > 4) {
-      $("#results").removeClass("hidden");
       fetchGeoocde($(this).val());
     } else {
       $("#search_btn").removeClass("disabled");
       $("#search").addClass("end");
     }
 
-    $("#search_btn").on("click", function () {
-      var loc = $("#location").val();
-      fetchData(loc, apiKey, lang);
+    // $("#search_btn").on("click", function () {
+    //   var loc = $("#location").val();
+    //   fetchData(loc, apiKey, lang);
+    //   $("#results").addClass("hidden");
+    // });
+
+    $("#results").on("click", ".searchbtn", function () {
       $("#results").addClass("hidden");
 
+      var lat = $(this).data("lat");
+      var lon = $(this).data("lon");
+      var name = $(this).data("name");
+      var country = $(this).data("country");
+      var state = $(this).data("state");
+
+      fetchData(lat, lon, name);
     });
   });
 });
 
-function fetchData(city, key, lang) {
-  var url =
-    "https://api.openweathermap.org/data/2.5/weather?q=" +
-    city +
-    "&appid=" +
-    key;
+function fetchData(latitude, longitude, name) {
+  if (!latitude || !longitude) {
+    throw new Error("Can't fetch data! Latitude, Longitude required.");
+  }
+
+  //  https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+  var url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
   fetch(url)
     .then(function (res) {
       return res.json();
     })
     .then(function (data) {
       console.log(data);
-      $("#city_name").text(data.name);
       $("#info").removeClass("hidden");
+
+      $("#city-name").text(data.name);
+      $("#country").text(data.sys.country);
+
+      $("#weather-main").text(data.weather[0].main);
+      $("#weather-desc").text(data.weather[0].description);
+      $("#weather-icon").attr(
+        "src",
+        `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+      );
+
+      // Imperial: F
+      // $("#temp").text((data.main.temp - 273.15).toFixed(1));
+      // $("#feels-like").text((data.main.feels_like - 273.15).toFixed(1));
+      // $("#temp-min").text((data.main.temp_min - 273.15).toFixed(1));
+      // $("#temp-max").text((data.main.temp_max - 273.15).toFixed(1));
+
+      // Metric: C
+      $("#temp").text(data.main.temp);
+      $("#feels-like").text(data.main.feels_like);
+      $("#temp-min").text(data.main.temp_min);
+      $("#temp-max").text(data.main.temp_max);
+
+      $("#wind-gust").text(data.wind.gust); // Imp: miles/hour  metric: meter/sec
+
+      $("#humidity").text(data.main.humidity);                                  // %
+      $("#pressure").text(data.main.pressure);                                  // hPa
+      $("#visibility").text(data.visibility);                                   // m
+
+      $("#wind-speed").text(data.wind.speed);                                   // meter/sec
+      $("#wind-deg").text(data.wind.deg);                                       // meteorological degrees
+
+      $("#clouds-all").text(data.clouds.all);                                   // %
+
+      // Convert timestamp to local time
+      $("#sunrise").text(
+        new Date(data.sys.sunrise * 1000).toLocaleTimeString()
+      );                                                                        // unix, UTC
+      $("#sunset").text(new Date(data.sys.sunset * 1000).toLocaleTimeString()); // unix, UTC
+    })
+    .catch(function (error) {
+      console.log(error);
     });
-  // .catch();
 }
 
 function fetchGeoocde(str) {
-  console.log(str);
+  $("#results ul").empty();
+  $("#resp").empty();
+
   var url1 =
     "http://api.openweathermap.org/geo/1.0/direct?q=" +
     str +
@@ -53,9 +115,41 @@ function fetchGeoocde(str) {
 
   fetch(url1)
     .then(function (res) {
+      if (!res.status) {
+        throw new Error("Unable to fetch results !");
+      }
       return res.json();
     })
     .then(function (data) {
-      console.log(data);
+      if (data.length === 0) {
+        throw new Error("No Data Available !");
+      }
+
+      var res = data.map((item) => ({
+        name: item.name || "",
+        local_names: item.local_names || {}, // ensure it's always an object
+        lat: item.lat || 0,
+        lon: item.lon || 0,
+        country: item.country || "",
+        state: item.state || "",
+      }));
+
+      $.each(res, function (idx, loc) {
+        var $li = $(`
+      <li class="searchbtn" data-lat="${loc.lat}" data-lon="${loc.lon}" data-name="${loc.name}" data-country="${loc.country}" data-state="${loc.state}">
+        <span class="name">${loc.name}</span>
+        <span class="state">${loc.state}</span>
+        <span class="country">${loc.country}</span>
+      </li>
+    `);
+        $("#results ul").append($li);
+      });
+
+      $("#results").removeClass("hidden");
+    })
+    .catch(function (error) {
+      $("#results").removeClass("hidden");
+      $("#resp").removeClass("hidden");
+      $("#resp").css("marginBottom", 0).text(error.message);
     });
 }
